eb52d1e3fe81cb8b47ab880027cb7969b837f598
Bernd Drechsel <bdrechsel@navis-ams.com>
2020-06-15 10:58:34 +0200

    Configured postgres db.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,12 +1,20 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
+def sample_user(email='test@londonappdev.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@lodonappdev.com'
+        email = 'test@londonappdev.com'
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -17,13 +25,13 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        """Test the emai for a new user is normalized"""
+        """Test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
-    def test_new_user_invalide_email(self):
+    def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
