332c162978e6e1758313aeedcc3496dac6a94c4d
Bernd Drechsel <bdrechsel@navis-ams.com>
2020-06-16 12:13:09 +0200

    Added create ingredient feature.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,7 +3,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-# from core import models
+from core import models
 
 
 def sample_user(email='test@londonappdev.com', password='testpass'):
@@ -49,7 +49,18 @@ class ModelTests(TestCase):
 
     def test_tag_str(self):
         """Test the tag string representation"""
-        # tag = models.Tag.objects.create(
-        #    user=sample_user(),
-        #    name='Vegan'
-        # )
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string respresentation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
